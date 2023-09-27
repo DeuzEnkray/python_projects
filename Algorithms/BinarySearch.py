a16d53dc8ee388712ea5fd91aea57ce29612d8dc
Deuz Enkray <143562280+DeuzEnkray@users.noreply.github.com>
2023-09-27 22:30:52 +0530

    Create BinarySearch.py

---
+++ Algorithms/BinarySearch.py
@@ -0,0 +1,19 @@
+# __gold-init__
+# Binary Search
+
+def binary_search(array, item):
+    low = 0
+    high = len(array) - 1
+    while low <= high:
+        mid = (low + high) // 2
+
+        if array[mid] == item:
+            return mid
+
+        elif array[mid] < item:
+            low = mid + 1
+
+        else:
+            high = mid - 1
+
+    return None
