a8bcef0e48768813fdf6da58e1725b1f39e8e98a
Deuz Enkray <143562280+DeuzEnkray@users.noreply.github.com>
2023-09-27 19:07:49 +0530

    Add files via upload

---
+++ Stack_LL.py
@@ -0,0 +1,44 @@
+# __gold-init
+# Stack implementation using Linked Lists
+
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+
+class Stack:
+    def __init__(self):
+        self.__top = None
+        self.__n = 0
+
+    def push(self, data):
+        node = Node(data)
+
+        if self.__top is None:
+            self.__top = node
+
+        else:
+            node.next = self.__top
+            self.__top = node
+
+        self.__n += 1
+
+    def pop(self):
+        if self.__top is None:
+            return None
+
+        else:
+            data = self.__top.data
+            self.__top = self.__top.next
+            self.__n -= 1
+            return data
+
+    def top(self):
+        return self.__top.data
+
+    def is_empty(self):
+        return self.__top is None
+
+    def size(self):
+        return self.__n
